982c402ba20d38ad4b5446e1d60df59e1dee087d
Ben Zeltser <zeltserben@Hotmail.com>
2024-01-02 18:45:18 +0200

    CSS Designs

--- src/Components/LoginSignUp/LoginSignup.jsx
+++ src/Components/LoginSignUp/LoginSignup.jsx
@@ -8,7 +8,28 @@ import password_icon from '../Assets/password.png'
 const LoginSignup = () => {
   return (
     <div className='container'>
-      
+      <div className="header">
+      <div className="text">Sign Up</div>
+      <div className="underline"></div></div>
+      <div className="inputs">
+        <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" />
+        </div>
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" />
+        </div>
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" />
+        </div>
+      </div>
+      <div className="forgot-passwor">Forgot Password? <span>Click Here</span></div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Login</div>
+      </div>
     </div>
   )
 }
