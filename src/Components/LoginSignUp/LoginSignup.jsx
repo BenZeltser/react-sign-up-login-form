0f97dc353c0f3af7892753172b86446398c32b09
Ben Zeltser <zeltserben@Hotmail.com>
2024-01-02 19:15:18 +0200

    Applied trenary functions logic on multiple buttons

--- src/Components/LoginSignUp/LoginSignup.jsx
+++ src/Components/LoginSignUp/LoginSignup.jsx
@@ -15,10 +15,11 @@ const LoginSignup = () => {
       <div className="text">{action}</div>
       <div className="underline"></div></div>
       <div className="inputs">
-        <div className="input">
+        {action==="Login"?<div></div>:        <div className="input">
             <img src={user_icon} alt="" />
             <input type="text" placeholder='Name'/>
-        </div>
+        </div>}
+
         <div className="input">
             <img src={email_icon} alt="" />
             <input type="email" placeholder='Email ID'/>
@@ -28,10 +29,10 @@ const LoginSignup = () => {
             <input type="password" placeholder='Password' />
         </div>
       </div>
-      <div className="forgot-password">Forgot Password? <span>Click Here</span></div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password? <span>Click Here</span></div>}
       <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"}>Login</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"}  onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
   )
